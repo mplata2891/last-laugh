48f5474b0ba61acb9d8dfe2b8a8d243cbe9e8446
tigerlynn11 <tigerlynn11@gmail.com>
2018-05-08 18:17:55 -0400

    started newGame function

--- LastLaugh/src/Utility.java
+++ LastLaugh/src/Utility.java
@@ -1,5 +1,22 @@
 public class Utility{
 
+  public String newGame{//will not compile
+    FileUtility fileTool = new FileUtility();
+    
+    //access the splashPage member of the tool object to display the game's title
+    fileTool.readFile("TextFiles/SplashScreens/Title.txt");
+    
+    //access the readFile member of the tool object to display the story's Prologue
+    fileTool.readFile("TextFiles/Story/Intro1.txt");
+    
+    //ask for user input (name)
+    
+
+    //access the readFile member of the tool object to display the story's
+    //first part of the intro
+    fileTool.readFile("TextFiles/Story/Intro2.txt");
+  }
+
   //gives random int in a given interval
   public int chance(int start, int end){
       return start + (int)(Math.random() * (end - start));

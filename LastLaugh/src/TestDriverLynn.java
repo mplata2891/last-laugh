4632e8d49395166942a9d84c516bed68fd0c1e03
tigerlynn11 <tigerlynn11@gmail.com>
2018-05-27 13:29:12 -0400

    added asks for input in my game driver

--- LastLaugh/src/TestDriverLynn.java
+++ LastLaugh/src/TestDriverLynn.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class TestDriverLynn{
 
@@ -7,8 +9,25 @@ public class TestDriverLynn{
     Scanner input = new Scanner(System.in);
     FileUtility fileTool = new FileUtility();
     
-    fileTool.splashPage("beatGame");
-
+    menuPrompt();
+    itemPrompt();
+    
+    
+    /*
+    public static menuSelect(String option){
+      switch(option){
+          
+      }//end of switch
+    }//end of menuSelection
+    */
   }//end main
+  
+  public static void menuPrompt(){
+      System.out.println("Select a menu type a number from 0-8 to make a selection.");
+    }
+    
+    public static void itemPrompt(){
+      System.out.println("Input the name of the item you wish to use.");  
+    }
   
 }//end class

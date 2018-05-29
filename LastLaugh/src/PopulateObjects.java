86f5703f703e9a0babfaa98c48ba50b0dd741b7d
NinjaBastardZero <36210404+mplata2891@users.noreply.github.com>
2018-05-29 12:00:53 -0700

    Filled out some text files

--- LastLaugh/src/PopulateObjects.java
+++ LastLaugh/src/PopulateObjects.java
@@ -1,8 +1,6 @@
 //declare imports
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class PopulateObjects{
